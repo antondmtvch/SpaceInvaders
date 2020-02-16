259f82afb3f5f56d151870be8ba788113b0c975c
Anton Teplyakov <antondmtvch@gmail.com>
2020-02-16 13:31:02 +0300

    Добавлено поле enemyFleet.

--- src/games/spaceinvaders/SpaceInvadersGame.java
+++ src/games/spaceinvaders/SpaceInvadersGame.java
@@ -1,6 +1,7 @@
 package games.spaceinvaders;
 
 import com.javarush.engine.cell.*;
+import games.spaceinvaders.gameobjects.EnemyFleet;
 import games.spaceinvaders.gameobjects.Star;
 
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ public class SpaceInvadersGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
     private List<Star> stars;
+    private EnemyFleet enemyFleet;
 
     @Override
     public void initialize() {
