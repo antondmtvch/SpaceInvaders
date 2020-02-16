1625aa82fed584703197aa4165ed708fec6b592f
Anton Teplyakov <antondmtvch@gmail.com>
2020-02-16 22:24:55 +0300

    Добавлен класс Bullet

---
+++ src/games/spaceinvaders/gameobjects/Bullet.java
@@ -0,0 +1,22 @@
+package games.spaceinvaders.gameobjects;
+
+import games.spaceinvaders.Direction;
+import games.spaceinvaders.ShapeMatrix;
+
+public class Bullet extends GameObject {
+
+    private int dy;
+    public boolean isAlive = true;
+
+    public Bullet(double x, double y, Direction direction) {
+        super(x, y);
+        super.setMatrix(ShapeMatrix.BULLET);
+        if (direction == Direction.UP) {
+            this.dy = -1;
+        } else this.dy = 1;
+    }
+
+    public void move() {
+        this.y = this.y + this.dy;
+    }
+}
