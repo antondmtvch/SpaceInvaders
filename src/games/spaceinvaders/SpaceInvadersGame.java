45208defcdce062f4a3b357f6f8e5d6bddd51907
Anton Teplyakov <antondmtvch@gmail.com>
2020-02-17 22:24:59 +0300

    Добавлены приватные поля COMPLEXITY, PLAYER_BULLETS_MAX

--- src/games/spaceinvaders/SpaceInvadersGame.java
+++ src/games/spaceinvaders/SpaceInvadersGame.java
@@ -11,6 +11,8 @@ public class SpaceInvadersGame extends Game {
 
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    public static final int COMPLEXITY = 5;
+    private static final int PLAYER_BULLETS_MAX = 1;
     private List<Star> stars;
     private EnemyFleet enemyFleet;
     public static final int COMPLEXITY = 5;
