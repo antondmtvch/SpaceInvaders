6d4f1313bd56915defdf6e5d8d65deaad437d017
Anton Teplyakov <antondmtvch@gmail.com>
2020-02-17 22:26:16 +0300

    Переопределены методы onKeyPress, onKeyReleased, setCellValueEx

--- src/games/spaceinvaders/SpaceInvadersGame.java
+++ src/games/spaceinvaders/SpaceInvadersGame.java
@@ -1,7 +1,9 @@
 package games.spaceinvaders;
 
 import com.javarush.engine.cell.*;
+import games.spaceinvaders.gameobjects.Bullet;
 import games.spaceinvaders.gameobjects.EnemyFleet;
+import games.spaceinvaders.gameobjects.PlayerShip;
 import games.spaceinvaders.gameobjects.Star;
 
 import java.util.ArrayList;
@@ -15,7 +17,13 @@ public class SpaceInvadersGame extends Game {
     private static final int PLAYER_BULLETS_MAX = 1;
     private List<Star> stars;
     private EnemyFleet enemyFleet;
-    public static final int COMPLEXITY = 5;
+    private List<Bullet> enemyBullets;
+    private List<Bullet> playerBullets;
+    private PlayerShip playerShip;
+    private boolean isGameStopped;
+    private int animationsCount;
+
+
 
     @Override
     public void initialize() {
@@ -24,9 +32,37 @@ public class SpaceInvadersGame extends Game {
     }
 
     @Override
+    public void onKeyPress(Key key) {
+        if (key == Key.SPACE && isGameStopped) createGame();
+        if (key == Key.LEFT) playerShip.setDirection(Direction.LEFT);
+        if (key == Key.RIGHT) playerShip.setDirection(Direction.RIGHT);
+        if (key == Key.SPACE) {
+            Bullet bullet = playerShip.fire();
+            if (bullet != null && playerBullets.size() < PLAYER_BULLETS_MAX) {
+                playerBullets.add(bullet);
+            }
+        }
+    }
+
+    @Override
+    public void onKeyReleased(Key key) {
+        if (key == Key.LEFT && playerShip.getDirection() == Direction.LEFT
+                || key == Key.RIGHT && playerShip.getDirection() == Direction.RIGHT) {
+            playerShip.setDirection(Direction.UP);
+        }
+    }
+
+    @Override
     public void onTurn(int step) {
         moveSpaceObjects();
         drawScene();
+    }
+
+    @Override
+    public void setCellValueEx(int x, int y, Color cellColor, String value) {
+        if (x > 0 && y > 0 && x < WIDTH && y < HEIGHT) {
+            super.setCellValueEx(x, y, cellColor, value);
+        }
     }
 
     private void createGame() {
