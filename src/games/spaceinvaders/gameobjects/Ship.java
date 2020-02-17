617a81c84e0c950dde70e01db241c51a6b7f31af
Anton Teplyakov <antondmtvch@gmail.com>
2020-02-17 22:29:12 +0300

    Обновлен метод setStaticView. Добавлены kill, setAnimatedView, nextFrame

--- src/games/spaceinvaders/gameobjects/Ship.java
+++ src/games/spaceinvaders/gameobjects/Ship.java
@@ -1,6 +1,16 @@
 package games.spaceinvaders.gameobjects;
 
+import com.javarush.engine.cell.Game;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Ship extends GameObject {
+
+    public boolean isAlive = true;
+    private List<int[][]> frames;
+    private int frameIndex;
 
     public Ship(double x, double y) {
         super(x, y);
@@ -14,9 +24,34 @@ public class Ship extends GameObject {
 
     public void setStaticView(int[][] viewFrame) {
         super.setMatrix(viewFrame);
+        this.frames = new ArrayList<int[][]>();
+        this.frameIndex = 0;
+        frames.add(viewFrame);
     }
 
     public Bullet fire() {
         return null;
+    }
+
+    public void kill() {
+        this.isAlive = false;
+    }
+
+    public void setAnimatedView(int[][]... viewFrames) {
+        super.setMatrix(viewFrames[0]);
+        this.frames = Arrays.asList(viewFrames);
+        this.frameIndex = 0;
+    }
+
+    public void nextFrame() {
+        this.frameIndex += 1;
+        if (this.frameIndex < this.frames.size()) {
+            this.matrix = frames.get(this.frameIndex);
+        }
+    }
+
+    public boolean isVisible() {
+        return this.isAlive || this.frameIndex < this.frames.size();
+
     }
 }
