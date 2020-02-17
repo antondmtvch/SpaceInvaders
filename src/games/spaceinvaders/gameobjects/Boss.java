e706012f9855fd40d1b249cd8ca9753503262183
Anton Teplyakov <antondmtvch@gmail.com>
2020-02-17 22:49:02 +0300

    Переопределен метод nextFrame

--- src/games/spaceinvaders/gameobjects/Boss.java
+++ src/games/spaceinvaders/gameobjects/Boss.java
@@ -4,8 +4,18 @@ import games.spaceinvaders.ShapeMatrix;
 
 public class Boss extends EnemyShip {
 
+    private int frameCount = 0;
+
     public Boss(double x, double y) {
         super(x, y);
         this.setAnimatedView(ShapeMatrix.BOSS_ANIMATION_FIRST, ShapeMatrix.BOSS_ANIMATION_SECOND);
+    }
+
+    @Override
+    public void nextFrame() {
+        this.frameCount += 1;
+        if (frameCount % 10 == 0 || !this.isAlive) {
+            super.nextFrame();
+        }
     }
 }
