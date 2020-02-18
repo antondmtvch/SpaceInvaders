7c02bb0a4b48bdf954ef8b157fa315a83e8cf3b1
Anton Teplyakov <antondmtvch@gmail.com>
2020-02-18 20:19:34 +0300

    Переопределены методы fire, kill. Добавлен вызов метода setAnimatedView в конструктор.

--- src/games/spaceinvaders/gameobjects/Boss.java
+++ src/games/spaceinvaders/gameobjects/Boss.java
@@ -1,5 +1,6 @@
 package games.spaceinvaders.gameobjects;
 
+import games.spaceinvaders.Direction;
 import games.spaceinvaders.ShapeMatrix;
 
 public class Boss extends EnemyShip {
@@ -8,12 +9,39 @@ public class Boss extends EnemyShip {
 
     public Boss(double x, double y) {
         super(x, y);
-        this.setAnimatedView(ShapeMatrix.BOSS_ANIMATION_FIRST, ShapeMatrix.BOSS_ANIMATION_SECOND);
+        this.score = 100;
+        this.setAnimatedView(true,
+                ShapeMatrix.BOSS_ANIMATION_FIRST,
+                ShapeMatrix.BOSS_ANIMATION_SECOND
+        );
+    }
+
+    @Override
+    public Bullet fire() {
+        if (!this.isAlive) {
+            return null;
+        }
+        if (this.matrix == ShapeMatrix.BOSS_ANIMATION_FIRST) {
+            return new Bullet(this.x + 6, this.y + this.height, Direction.DOWN);
+        }
+        return new Bullet(this.x, this.y + this.height, Direction.DOWN);
+    }
+
+    @Override
+    public void kill() {
+        if (this.isAlive) {
+            this.isAlive = false;
+            super.setAnimatedView(false,
+                    ShapeMatrix.KILL_BOSS_ANIMATION_FIRST,
+                    ShapeMatrix.KILL_BOSS_ANIMATION_SECOND,
+                    ShapeMatrix.KILL_BOSS_ANIMATION_THIRD
+            );
+        }
     }
 
     @Override
     public void nextFrame() {
-        this.frameCount += 1;
+        this.frameCount++;
         if (frameCount % 10 == 0 || !this.isAlive) {
             super.nextFrame();
         }
